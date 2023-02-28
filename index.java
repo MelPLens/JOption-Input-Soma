48db2e7561c0d7ccd4bc2a3a7e11c6d659222978
MEL PLENS ANGELIS <99374140+MelPLens@users.noreply.github.com>
2023-02-27 21:39:23 -0300

    Create index.java

---
+++ index.java
@@ -0,0 +1,18 @@
+package caixa.de.dialogo;
+import javax.swing.JOptionPane;
+
+public class CaixaDeDialogo {
+    
+    public static void main(String[] args) {
+        
+        int num1, num2, resultado;
+        num1= Integer.parseInt(JOptionPane.showInputDialog("insira o primeiro numero")); // o integer é para converter string para int(inteiro)
+        num2= Integer.parseInt(JOptionPane.showInputDialog("insira o segundo numero"));
+        resultado = num1+num2;
+        System.out.println("O resultado da soma é :"+ resultado);
+        JOptionPane.showMessageDialog(null,"O Resultado da soma é :" + resultado);    
+        
+     
+    }
+    
+}
