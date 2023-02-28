66636432a4d6d77d29a91b239279c5ca7c762b95
MEL PLENS ANGELIS <99374140+MelPLens@users.noreply.github.com>
2023-02-27 22:30:10 -0300

    Create if.java

---
+++ if.java
@@ -0,0 +1,21 @@
+
+package caixa.de.dialogo;
+import javax.swing.JOptionPane;
+
+public class CaixaDeDialogo {
+    
+    public static void main(String[] args) {
+        
+        int num1, num2, resultado;
+        String resposta;
+        Object[] Itens= {"ADS","Gestao de TI","Banco de dados"};// object seria um array
+        resposta= String.valueOf(
+        JOptionPane.showInputDialog(null,"Escolha um curso","CURSOS FACENS",// subtitulo e titulo da caixa
+        JOptionPane.PLAIN_MESSAGE,null,Itens,"ADS");// sem icone
+        );
+      If //mostrar que selecionou
+        
+     
+    }
+    
+}
