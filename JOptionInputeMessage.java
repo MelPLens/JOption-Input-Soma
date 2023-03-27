c0d5dc625f690fdf9c5fb2280c479a8b0ac3c5e5
MEL PLENS ANGELIS <99374140+MelPLens@users.noreply.github.com>
2023-03-26 22:01:49 -0300

    Update JOptionInputeMessage.java

--- JOptionInputeMessage.java
+++ JOptionInputeMessage.java
@@ -1,17 +1,20 @@
-package caixa.de.dialogo;
+package Joptionpane;
 import javax.swing.JOptionPane;
 
-public class CaixaDeDialogo {
+public class ExerciciosPane {
+    
     
     public static void main(String[] args) {
         
-        int num1, num2, resultado;
-        num1= Integer.parseInt(JOptionPane.showInputDialog("insira o primeiro numero")); // o integer é para converter string para int(inteiro)
-        num2= Integer.parseInt(JOptionPane.showInputDialog("insira o segundo numero"));
+        int num1, num2, resultado; // int do tipo primitivo
+        num1= Integer.parseInt(JOptionPane.showInputDialog("Insira o primeiro numero")); // o integer é para converter string para int(inteiro)
+        num2= Integer.parseInt(JOptionPane.showInputDialog("Insira o segundo numero"));
         resultado = num1+num2;
         System.out.println("O resultado da soma é :"+ resultado);
-        JOptionPane.showMessageDialog(null,"O Resultado da soma é :" + resultado);    
+         JOptionPane.showMessageDialog(null,"O Resultado da soma é :" + resultado, "RESULTADO",JOptionPane.QUESTION_MESSAGE); 
+        // apos a virgula do resultado, é colocado o " RESULTADO" no qual seria o tirulo,JoptionPane mais o icone para adiocionar na caixa
         
+ 
      
     }
     
